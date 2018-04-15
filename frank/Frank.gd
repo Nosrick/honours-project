2edfa92f77444b0ba11d87ee3d7abbb9b66eb4df
nosrick <nosrick@gmail.com>
2018-04-15 12:39:18 +0100

    Added start of Frank

---
+++ frank/Frank.gd
@@ -0,0 +1,84 @@
+extends Node
+ 
+var node = preload("FrankNode.gd")
+var tools = load("res://Tools.gd").new()
+ 
+var filePath = "res://myBrainFrank.json"
+ 
+var nodes = []
+var width
+ 
+var learningRate = 0.3
+ 
+func _init(widthRef):
+  randomize()
+  nodes = []
+  width = widthRef
+  
+  for x in range(width):
+    nodes.append(node.new())
+ 
+func Epoch(newNode):
+  var node = GetBestMatch(newNode)
+  var influence = 0.5
+  node.AdjustMana(newNode.targetMana, learningRate, influence)
+  node.AdjustQWeight(newNode.qWeight, learningRate, influence)
+ 
+func GetBestMatch(input):
+  var lowestDistance = 9999999
+  var winner = null
+  
+  for node in nodes:
+    if input.castingCardID == node.castingCardID:
+      var distance = node.GetDistanceMana(input.targetMana)
+      if distance < lowestDistance:
+        lowestDistance = distance
+        winner = node
+      
+  return winner
+ 
+func GetBestQScore(input):
+  var highestQScore = 0
+  var winner = null
+  
+  for node in nodes:
+    if input.castingCardID == node.castingCardID:
+      if node.qWeight > highestQScore:
+        highestQScore = node.qWeight
+  for node in nodes:
+    var nodeData = node.Save()
+    brain.store_line(nodeData.to_json())
+  
+  brain.close()
+  print("DONE SERIALISING")
+  
+func Deserialise():
+  var brain = File.new()
+  if not brain.file_exists(filePath):
+    return false
+  
+  brain.open(filePath, File.READ)
+  
+  nodes = []
+  
+  var currentLine = {}
+  while(!brain.eof_reached()):
+    currentLine.parse_json(brain.get_line())
+    var newNode = node.new()
+    newNode.castingCardID = currentLine.castingCardID
+    newNode.castingCardType = currentLine.castingCardType
+    newNode.targetMana = currentLine.targetMana
+    #newNode.weight = currentLine.weight
+    newNode.qWeight = currentLine.qWeight
+    
+    nodes.append(newNode)
+  
+  brain.close()
+  width = nodes.size()
+  return true
+ 
+func ExtractVector(string):
+  var xIndex = string.find(",")
+  var x = string.substr(1, xIndex - 1)
+  var y = string.substr(xIndex + 1, string.length() - 1)
+  return Vector2(float(x), float(y))
---
+++ frank/FrankNode.gd
@@ -0,0 +1,54 @@
+extends Node
+ 
+var castingCardID
+var castingCardType
+var targetMana
+var qWeight
+ 
+var tools = load("res://Tools.gd").new()
+ 
+func _init():
+	castingCardID = "None"
+	qWeight = randf()
+	targetMana = 1
+ 
+func SetParameters(node):
+	castingCardID = node.castingCardID
+	castingCardType = node.castingCardType
+	targetMana = node.targetMana
+ 
+func GetDistanceMana(targetManaRef):
+	var distance = (targetManaRef - targetMana) * (targetManaRef - targetMana)  
+
+	distance = sqrt(distance)
+	return distance
+ 
+func AdjustMana(targetManaRef, learningRate, influence):
+	#OJA-STYLE LEARNING
+	var normalisedMana = tools.NormaliseOneToTen(targetMana)
+	var normalisedTarget = tools.NormaliseOneToTen(targetManaRef)
+	var mana = float(normalisedTarget * (normalisedMana * normalisedTarget)) - ((normalisedMana * normalisedTarget) * (normalisedMana * normalisedTarget) * normalisedMana)
+	if mana == 0:
+		mana = normalisedTarget
+  
+	var recombobulated = float(tools.RecombobulateOneToTen(mana))
+  
+	targetMana += float(float(influence) * float(learningRate) * recombobulated)
+
+func AdjustQWeight(qWeightRef, learningRate, influence):
+	#OJA-STYLE LEARNING
+	var newWeight = float(influence * float(learningRate * float((qWeightRef * (qWeight * qWeightRef)) - ((qWeight * qWeightRef) * (qWeight * qWeightRef) * qWeight))))
+  
+	qWeight += newWeight
+ 
+func ToString():
+	return "[" + castingCardID + " : " + str(castingCardType) + " : " + str(targetMana) + " : " + str(qWeight) + "]"
+ 
+func Save():
+	var data = {}
+	data.castingCardID = castingCardID
+	data.castingCardType = castingCardType
+	data.targetMana = targetMana
+	data.qWeight = qWeight
+	  
+	return data
